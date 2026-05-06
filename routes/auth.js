4ff9e3753374c0a5657b5f2fdb0b3b214a972430
zakingeric <osilamaeric@gmail.com>
2026-05-06 21:51:51 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const rateLimit = require('express-rate-limit');
 
 const db        = require('../config/database');
 const logger    = require('../config/logger');
-const { protect } = require('../routes/auth');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
