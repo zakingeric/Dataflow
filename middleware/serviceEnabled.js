22ee2880950dc94741436b7acd9d2f2bf7859b28
zakingeric <osilamaeric@gmail.com>
2026-05-08 12:02:58 +0100

    Update serviceEnabled.js

--- middleware/serviceEnabled.js
+++ middleware/serviceEnabled.js
@@ -1,7 +1,11 @@
-const serviceEnabled = (serviceName) => {
+module.exports = function serviceEnabled(flag) {
   return (req, res, next) => {
+    if (!process.env[flag.toUpperCase()]) {
+      return res.status(503).json({
+        success: false,
+        message: 'Service disabled'
+      });
+    }
     next();
   };
 };
-
-module.exports = { serviceEnabled };
